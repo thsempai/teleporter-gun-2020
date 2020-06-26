4b1860f12dadb0d93a937c05a8dc228449472145
Thomas Stassin <thomas@panopticgame.com>
2020-06-26 14:05:49 +0200

    add teleporter

---
+++ Teleporter Gun 2020/Assets/#Project/Scripts/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    private GameObject player;
+    public Transform teleportPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Projectile") {
+            CharacterController cc = player.GetComponent<CharacterController>();
+
+            cc.enabled = false;
+            player.transform.position = teleportPoint.position;
+            cc.enabled = true;
+        }
+    }       
+}
