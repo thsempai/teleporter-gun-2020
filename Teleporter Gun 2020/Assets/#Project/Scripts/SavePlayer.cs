728a9711a89ffa12279e85f8b5ce68705a2dda1a
Thomas Stassin <thomas@panopticgame.com>
2020-06-26 14:55:48 +0200

    add first level

---
+++ Teleporter Gun 2020/Assets/#Project/Scripts/SavePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SavePlayer : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        originalPosition = player.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            CharacterController cc = player.GetComponent<CharacterController>();
+            cc.enabled = false;
+            player.transform.position = originalPosition;
+            cc.enabled = true;
+        }
+
+    }
+}
