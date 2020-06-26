1a7841f28d4362f2fd63902e85db2ecfc60002d4
Thomas Stassin <thomas@panopticgame.com>
2020-06-26 12:15:47 +0200

    add gun

---
+++ Teleporter Gun 2020/Assets/#Project/Scripts/FireGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform spawnPoint;
+
+    private void Fire() {
+        Vector3 position = spawnPoint.position;
+        Quaternion rotation = spawnPoint.rotation;
+
+        Instantiate(projectile, position, rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Fire();
+        }
+    }
+}
---
+++ Teleporter Gun 2020/Assets/#Project/Scripts/StartImpulse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StartImpulse : MonoBehaviour
+{
+    public float force = 1000f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
